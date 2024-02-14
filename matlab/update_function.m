caa439802185301995b31cd880c853f1e799e3c6
wherzberg <70026035+wherzberg@users.noreply.github.com>
2024-02-14 13:32:48 -0600

    Update update_function.m
    removed personal directories

--- matlab/update_function.m
+++ matlab/update_function.m
@@ -2,7 +2,7 @@ function del_sigmas = update_function(sigmas, filename)
     % Sigmas is an n by m array containing conductivity distributions for n
     % samples defined over m elements. filename contains the rest of the
     % data needed to compute updates (the model and measurement data)
-    run 'C:\Users\Billy\OneDrive - Marquette University\EIDORS\eidors-v3.10-ng\eidors\startup';
+    run 'path_to_startup\eidors\startup';
 
     % Load the extra data
     load(filename, "VSIM", "rec_fmdl");
